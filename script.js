4350180999290a8a4a4103dd44724e8042d54ed0
Nunes025 <131490722+Nunes025@users.noreply.github.com>
2025-04-27 16:58:34 -0300

    script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById('surveyForm').addEventListener('submit', function(event)
     const satisfaction = document.getElementById('satisfaction').value;
     const comments = document.getElementById('comments').value;
 
-    fetch('https://script.google.com/macros/s/AKfycbx7lrJNOAXMnNGDm0gbEMUb89hPktdjiwwdcvLEBoo/dev', {
+    fetch('https://script.google.com/macros/s/AKfycbwUIVWdAF_LMGwCQM1EubjEVUMlik6eivxiaBX6reduYmMrlfIzT3vg8K8m2iu6TdYK/exec', {
         method: 'POST',
         body: JSON.stringify({
             satisfaction: satisfaction,
