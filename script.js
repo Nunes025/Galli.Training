5c83fae74e0b465d8bb70bc62770785734e2ca67
Nunes025 <131490722+Nunes025@users.noreply.github.com>
2025-04-27 16:47:46 -0300

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 document.getElementById('surveyForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Impede o envio do formulário
+    event.preventDefault(); // Impede o envio normal do formulário
 
-    // Coleta os dados do formulário
     const satisfaction = document.getElementById('satisfaction').value;
     const comments = document.getElementById('comments').value;
 
-    // Aqui você pode enviar os dados para um backend (ex: API) para armazenar
-    // Para este exemplo, vamos apenas exibir uma mensagem de sucesso
-    const messageDiv = document.getElementById('message');
-    messageDiv.innerHTML = `<p>Obrigado por sua participação!</p>`;
-
-    // Limpa o formulário
-    document.getElementById('surveyForm').reset();
+    fetch('https://script.google.com/macros/s/AKfycbwUIVWdAF_LMGwCQM1EubjEVUMlik6eivxiaBX6reduYmMrlfIzT3vg8K8m2iu6TdYK/exec', {
+        method: 'POST',
+        body: JSON.stringify({
+            satisfaction: satisfaction,
+            comments: comments
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(response => {
+        if (response.ok) {
+            const messageDiv = document.getElementById('message');
+            messageDiv.innerHTML = `<p>Obrigado por sua participação!</p>`;
+            document.getElementById('surveyForm').reset();
+        } else {
+            alert('Erro ao enviar. Tente novamente.');
+        }
+    }).catch(error => {
+        alert('Erro de conexão.');
+        console.error(error);
+    });
 });
